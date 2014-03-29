735a42cf7e8c9879740116fb3ba6b0ee35c54271
Ubuntu <ubuntu@ip-172-31-46-125.us-west-2.compute.internal>
2014-03-29 21:55:37 +0000

    running on flask

--- backtestapp.py
+++ backtestapp.py
@@ -22,10 +22,10 @@ def _do_backtest():
     return data
 
 @app.route('/')
-def index():
+def backtestapp():
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug=True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=80)
     
